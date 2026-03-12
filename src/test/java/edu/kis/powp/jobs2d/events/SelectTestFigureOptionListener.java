1a18b63a5ced66a9739822030210d64684ea70f3
Sebastian Cieslicki <251132@edu.p.lodz.pl>
2026-03-12 11:39:16 +0100

    Added Figure Joe 2

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -9,13 +9,24 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 public class SelectTestFigureOptionListener implements ActionListener {
 
 	private DriverManager driverManager;
+	private int figureNumber;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
+
+	public SelectTestFigureOptionListener(DriverManager driverManager, int figureNumber) {
 		this.driverManager = driverManager;
+		this.figureNumber = figureNumber;
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+	public void actionPerformed(ActionEvent e)
+	{
+
+
+		if(figureNumber == 1) {
+			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		}
+		else {
+			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		}
 	}
 }
