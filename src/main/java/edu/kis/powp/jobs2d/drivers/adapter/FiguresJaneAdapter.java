c1f2ee6ae1e33f68e4159923baff009f0e8a3daf
Sebastian Cieslicki <251132@edu.p.lodz.pl>
2026-03-15 20:36:31 +0100

    Added FigurJaneAdapter

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/FiguresJaneAdapter.java
@@ -0,0 +1,26 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class FiguresJaneAdapter extends AbstractDriver {
+    private final Job2dDriver driver;
+
+    public FiguresJaneAdapter(Job2dDriver driver) {
+        super(0,0);
+        this.driver = driver;
+    }
+
+    @Override
+    public void operateTo(int x, int y){
+        driver.setPosition(getX(), getY());
+        driver.operateTo(x,y);
+        this.setPosition(x,y);
+    }
+
+    @Override
+    public String toString(){
+        return "Figure Jane Adapter";
+    }
+}
